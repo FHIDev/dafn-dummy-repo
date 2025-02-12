3933a3aff9de738e2bec31417a78febf1e9b816a
Daniel Nilsen <dafn@outlook.com>
2025-02-12 11:43:43 +0100

    fix: access control

--- packages/fhi-designsystem/vite.config.js
+++ packages/fhi-designsystem/vite.config.js
@@ -25,6 +25,10 @@ export default defineConfig(({ mode }) => {
                 src: 'src/theme',
                 dest: './',
               },
+              {
+                src: 'staticwebapp.config.json',
+                dest: './',
+              },
             ],
           }),
         ],
