922059fc80c432f4dba90c423a4865c474a274f7
Daniel Nilsen <dafn@Daniels-MBP.home>
2025-03-26 23:38:55 +0100

    nfjiawnji

--- packages/fhi-designsystem/web-test-runner.config.mjs
+++ packages/fhi-designsystem/web-test-runner.config.mjs
@@ -5,7 +5,7 @@ export default {
   files: ['**/*.test.ts'],
   plugins: [esbuildPlugin({ ts: true, tsconfig: 'tsconfig.json' })],
   nodeResolve: true,
-  concurrentBrowsers: 3,
+  concurrentBrowsers: 1,
   browsers: [
     playwrightLauncher({
       product: 'chromium',
