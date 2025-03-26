c1770a753e45ffdac815d5ab3cbb541a05389ced
Daniel Nilsen <dafn@Daniels-MBP.home>
2025-03-26 22:21:23 +0100

    turn off sandbox for browsers

--- packages/fhi-designsystem/web-test-runner.config.mjs
+++ packages/fhi-designsystem/web-test-runner.config.mjs
@@ -1,6 +1,21 @@
-import { esbuildPlugin } from "@web/dev-server-esbuild";
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default {
-  files: ["**/*.test.ts"],
-  plugins: [esbuildPlugin({ ts: true, tsconfig: "tsconfig.json" })],
+  files: ['**/*.test.ts'],
+  plugins: [esbuildPlugin({ ts: true, tsconfig: 'tsconfig.json' })],
+  browsers: [
+    playwrightLauncher({
+      launchOptions: { args: ['--no-sandbox'] },
+      product: 'chromium',
+    }),
+    playwrightLauncher({
+      launchOptions: { args: ['--no-sandbox'] },
+      product: 'firefox',
+    }),
+    playwrightLauncher({
+      launchOptions: { args: ['--no-sandbox'] },
+      product: 'webkit',
+    }),
+  ],
 };
