ea81e008f5b265c159174f2bb2e245d8759f5915
Daniel Nilsen <dafn@Daniels-MacBook-Pro.local>
2025-05-14 13:05:22 +0200

    angular formclontrol integration

--- app.angular/src/app/app.component.ts
+++ app.angular/src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+import '@folkehelseinstituttet/designsystem/fhi-text-input';
+import '@folkehelseinstituttet/designsystem/fhi-button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'app.angular';
+
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
+  handleSubmit() {
+    console.log(this.profileForm.value);
+  }
 }
