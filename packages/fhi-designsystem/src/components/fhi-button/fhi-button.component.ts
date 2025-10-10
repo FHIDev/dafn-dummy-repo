daf5923847bf88c9ea85b5b2554d0b17634583ba
HenrikFalla <henrik.falla@fhi.no>
2025-10-10 11:56:00 +0200

    Update fhi-button.component.ts

--- packages/fhi-designsystem/src/components/fhi-button/fhi-button.component.ts
+++ packages/fhi-designsystem/src/components/fhi-button/fhi-button.component.ts
@@ -11,7 +11,7 @@ export class FhiButton extends LitElement {
     | 'accent'
     | 'neutral'
     | 'something'
-    | 'something-nother515'
+    | 'something-nother5156'
     | 'danger' = 'accent';
 
   @property({ type: String, reflect: true }) variant:
